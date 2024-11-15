d653393aedc7ea825f3b488037d73b4b75fa11c1
Denis Jesus Palma Abanto <65937866+djesuspal@users.noreply.github.com>
2024-11-14 23:48:04 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="canonical" href="https://denispalma.dev" />
+        <link rel="canonical" href="https://denis.codes" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="index, follow" />
       </head>
